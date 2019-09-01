abdd0dd24b3d500bec0ec790370c512c25a709ed
Snezana Ilic <snezana.ilic@outlook.com>
2019-09-01 10:44:10 +0200

    Update dbconst.php
    removing webhost credentials from this file :))

--- dbconst.php
+++ dbconst.php
@@ -1,15 +1,8 @@
-<?php 
-
-	// const DB_HOST = 'mysql20.unoeuro.com';
-	// const DB_USER = 'sneska_dk';
-	// const DB_PASS = 'mangolove5';
-	// const DB_NAME = 'sneska_dk_db';
-	
+<?php	
 	const DB_HOST = 'localhost';
 	const DB_USER = 'root';
 	const DB_PASS = 'root';
 	const DB_NAME = 'phplogin';
-
 
 	$link = new mysqli( DB_HOST, DB_USER, DB_PASS ,DB_NAME );
 
